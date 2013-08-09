61bea94a8d9f7bc7fc31efb519ec228af1534ea9
Denver Coneybeare <denver@sleepydragon.org>
2013-08-09 16:44:28 -0400

    added build32-release.bat and build64-release.bat, to perform release builds instead of debug builds

--- build32-release.bat
+++ build32-release.bat
@@ -1,4 +1,4 @@
-@REM Compile 32-bit Debug Python from source
+@REM Compile 32-bit Release Python from source
 
 @REM Verify a valid build environment
 call %~dp0scripts\verify_build_env.bat
@@ -9,29 +9,29 @@ call %~dp0scripts\verify_build_env_32bit.bat
 @REM Compile TCL
 cd tcl8.5.14\win
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=1 INSTALLDIR=..\..\tcltk clean all
+nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=0 INSTALLDIR=..\..\tcltk clean all
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-nmake -f makefile.vc DEBUG=1 INSTALLDIR=..\..\tcltk install
+nmake -f makefile.vc DEBUG=0 INSTALLDIR=..\..\tcltk install
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
 cd ..\..
 
 @REM Compile Tk
 cd tk8.5.14\win
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=1 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl8.5.14 clean
+nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=0 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl8.5.14 clean
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=1 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl8.5.14 all
+nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=0 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl8.5.14 all
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=1 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl8.5.14 install
+nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=0 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl8.5.14 install
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
 cd ..\..
 
 @REM Compile Python
 cd Python-2.7.5\PCbuild
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-msbuild pcbuild.sln  /p:Configuration="Debug" /p:Platform="Win32"
+msbuild pcbuild.sln  /p:Configuration="Release" /p:Platform="Win32"
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-python_d -c "print('If you see this line then compilation succeeded')"
+python -c "print('If you see this line then compilation succeeded')"
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
 cd ..\..
 
@@ -47,4 +47,4 @@ exit /b 1
 @echo off
 echo.
 echo Python Successfully Built
-echo Try your newly-compiled Python interpreter by running: Python-2.7.5\PCbuild\python_d.exe
+echo Try your newly-compiled Python interpreter by running: Python-2.7.5\PCbuild\python.exe
--- build64-release.bat
+++ build64-release.bat
@@ -1,4 +1,4 @@
-@REM Compile 64-bit Debug Python from source
+@REM Compile 64-bit Release Python from source
 
 @REM Verify a valid build environment
 call %~dp0scripts\verify_build_env.bat
@@ -9,29 +9,29 @@ call %~dp0scripts\verify_build_env_64bit.bat
 @REM Compile TCL
 cd tcl8.5.14\win
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=1 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 clean all
+nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=0 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 clean all
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=1 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 install
+nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=0 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 install
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
 cd ..\..
 
 @REM Compile Tk
 cd tk8.5.14\win
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=1 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl8.5.14 clean
+nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=0 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl8.5.14 clean
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=1 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl8.5.14 all
+nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=0 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl8.5.14 all
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=1 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl8.5.14 install
+nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=0 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl8.5.14 install
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
 cd ..\..
 
 @REM Compile Python
 @REM For 64-bit builds, need to set HOST_PYTHON as a workaround for the way that VS invokes build_ssl.py
-set HOST_PYTHON=%~dp0Python-2.7.5\PCbuild\amd64\python_d.exe
+set HOST_PYTHON=%~dp0Python-2.7.5\PCbuild\amd64\python.exe
 cd Python-2.7.5\PCbuild
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
-msbuild pcbuild.sln  /p:Configuration="Debug" /p:Platform="x64"
+msbuild pcbuild.sln  /p:Configuration="Release" /p:Platform="x64"
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
 amd64\python_d -c "print('If you see this line then compilation succeeded')"
 @if %ERRORLEVEL% NEQ 0 goto BuildFailed
@@ -49,4 +49,4 @@ exit /b 1
 @echo off
 echo.
 echo Python Successfully Built
-echo Try your newly-compiled Python interpreter by running: Python-2.7.5\PCbuild\amd64\python_d.exe
+echo Try your newly-compiled Python interpreter by running: Python-2.7.5\PCbuild\amd64\python.exe
