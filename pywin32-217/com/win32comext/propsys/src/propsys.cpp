756aac3b145f9304a3096be5f506c11a5501c3f7
Denver Coneybeare <denver@sleepydragon.org>
2013-08-08 21:47:00 -0400

    pywin32-217 added source downloaded from http://hivelocity.dl.sourceforge.net/project/pywin32/pywin32/Build%20217/pywin32-217.zip (linked to from http://sourceforge.net/projects/pywin32/files/pywin32/Build%20217/pywin32-217.zip/download)

---
+++ pywin32-217/com/win32comext/propsys/src/propsys.cpp
@@ -0,0 +1,88 @@
+// propsys.cpp :
+// $Id$
+
+// Interfaces that support the Vista IProperty* interfaces.
+// Although propsys.dll existed before Vista, this module didn't - so we
+// don't LoadLibrary() stuff that only exist in Vista - sue me/or back-port
+// if you care :)
+
+// This source file contains autoduck documentation.
+// @doc
+
+// Any python API functions that use 's#' format must use Py_ssize_t for length
+#define PY_SSIZE_T_CLEAN
+
+#include "PythonCOM.h"
+#include "PythonCOMRegister.h"
+#include "propsys.h"
+
+// @pymethod |propsys|PSRegisterPropertySchema|
+static PyObject *PyPSRegisterPropertySchema(PyObject *self, PyObject *args)
+{
+	PyObject *obfname;
+	// @pyparm unicode|filename||
+	if (!PyArg_ParseTuple(args, "O:PSRegisterPropertySchema", &obfname))
+		return NULL;
+	WCHAR *sz;
+	if (!PyWinObject_AsWCHAR(obfname, &sz, FALSE))
+		return FALSE;
+	HRESULT hr;
+	PY_INTERFACE_PRECALL;
+	hr = PSRegisterPropertySchema(sz);
+	PY_INTERFACE_POSTCALL;
+	PyWinObject_FreeWCHAR(sz);
+	if (FAILED(hr))
+		return PyCom_BuildPyException(hr);
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+// @pymethod |propsys|PSUnregisterPropertySchema|
+static PyObject *PyPSUnregisterPropertySchema(PyObject *self, PyObject *args)
+{
+	PyObject *obfname;
+	// @pyparm unicode|filename||
+	if (!PyArg_ParseTuple(args, "O:PSUnregisterPropertySchema", &obfname))
+		return NULL;
+	WCHAR *sz;
+	if (!PyWinObject_AsWCHAR(obfname, &sz, FALSE))
+		return FALSE;
+	HRESULT hr;
+	PY_INTERFACE_PRECALL;
+	hr = PSUnregisterPropertySchema(sz);
+	PY_INTERFACE_POSTCALL;
+	PyWinObject_FreeWCHAR(sz);
+	if (FAILED(hr))
+		return PyCom_BuildPyException(hr);
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+/* List of module functions */
+// @module propsys|A module, encapsulating the Vista Property System interfaces
+static struct PyMethodDef propsys_methods[]=
+{
+	{ "PSRegisterPropertySchema", PyPSRegisterPropertySchema, 1 }, // @pymeth PyPSRegisterPropertySchema|
+	{ "PSUnregisterPropertySchema", PyPSUnregisterPropertySchema, 1 }, // @pymeth PyPSRegisterPropertySchema|
+	{ NULL, NULL },
+};
+
+
+//static const PyCom_InterfaceSupportInfo g_interfaceSupportData[] =
+//{
+//};
+
+/* Module initialisation */
+PYWIN_MODULE_INIT_FUNC(propsys)
+{
+	PYWIN_MODULE_INIT_PREPARE(propsys, propsys_methods,
+	                          "A module, encapsulating the Vista Property System interfaces");
+
+	if (PyDict_SetItemString(dict, "error", PyWinExc_COMError) == -1)
+		PYWIN_MODULE_INIT_RETURN_ERROR;
+
+	// Register all of our interfaces, gateways and IIDs.
+	//PyCom_RegisterExtensionSupport(dict, g_interfaceSupportData, sizeof(g_interfaceSupportData)/sizeof(PyCom_InterfaceSupportInfo));
+
+	PYWIN_MODULE_INIT_RETURN_SUCCESS;
+}
