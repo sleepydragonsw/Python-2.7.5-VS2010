0480c074fd557f2a006b6124861c21d2e759207a
Denver Coneybeare <denver@sleepydragon.org>
2013-08-09 16:01:23 -0400

    check for proper VS2010 environment in build32-debug.bat and build64-debug.bat

--- build64-debug.bat
+++ build64-debug.bat
@@ -1,5 +1,14 @@
 @REM Compile 32-bit Python from source
 
+@REM Ensure that we are running from a Visual Studio command prompt
+if "%VCINSTALLDIR%" == "" goto NotInVisualStudioCommandPrompt
+
+@REM Ensure that we are running from a 64-bit Visual Studio command prompt
+if "%Platform%" == "x64" goto In64BitVisualStudioCommandPrompt
+if "%Platform%" == "X64" goto In64BitVisualStudioCommandPrompt
+goto NotIn64BitVisualStudioCommandPrompt
+:In64BitVisualStudioCommandPrompt
+
 @REM Ensure that perl.exe is available; it is needed to compile OpenSSL
 perl -v >NUL
 @if %ERRORLEVEL% NEQ 0 goto NoPerl
@@ -42,17 +51,35 @@ cd ..\..
 
 goto done
 
+:NotInVisualStudioCommandPrompt
+@echo off
+echo.
+echo ERROR: Not running from a 64-bit Visual Studio command prompt. >&2
+echo Try selecting "Visual Studio x64 Win64 Command Prompt (2010)" or >&2
+echo "Visual Studio x64 Cross Tools Command Prompt (2010)" from the >&2
+echo Start menu and running this command from the resulting command prompt >&2
+goto BuildFailed
+
+:NotIn64BitVisualStudioCommandPrompt
+@echo off
+echo.
+echo ERROR: Running from a 32-bit Visual Studio command prompt, but 64-bit required. >&2
+echo Try selecting "Visual Studio x64 Win64 Command Prompt (2010)" or >&2
+echo "Visual Studio x64 Cross Tools Command Prompt (2010)" from the >&2
+echo Start menu and running this command from the resulting command prompt >&2
+goto BuildFailed
+
 :NoPerl
 @echo off
 echo.
-echo ERROR: perl.exe is required in order to compile OpenSSL >&2
+echo ERROR: perl.exe is required in order to compile OpenSSL. >&2
 echo Try installing ActivePerl (eg. ActivePerl-5.16.3.1603-MSWin32-x86) >&2
 goto BuildFailed
 
 :NoNasm
 @echo off
 echo.
-echo ERROR: nasm.exe is required in order to compile OpenSSL >&2
+echo ERROR: nasm.exe is required in order to compile OpenSSL. >&2
 echo Version 2.10.1 or greater is required >&2
 echo Try installing Nasm from http://www.nasm.us/pub/nasm/releasebuilds/2.10.09/win32 >&2
 goto BuildFailed
